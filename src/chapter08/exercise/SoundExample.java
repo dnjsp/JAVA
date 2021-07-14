fe62d60334bffe6faffa6d882571aba4dff7f441
PC-13 <PC-13@DESKTOP-JQN4RP0>
2021-07-14 17:17:41 +0900

    chapter09

---
+++ src/chapter08/exercise/SoundExample.java
@@ -0,0 +1,12 @@
+package chapter08.exercise;
+
+public class SoundExample {
+	public static void main(String[] args) {
+		printSound(new Cat());
+		printSound(new Dog());
+	}
+	
+	public static void printSound(Soundable soundable) {
+		System.out.println(soundable.sound());
+	}
+}
