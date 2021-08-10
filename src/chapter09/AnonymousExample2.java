3822a881f7a3904faadc02263cb5709dec349d0e
dnjsp <87220653+dnjsp@users.noreply.github.com>
2021-08-10 12:36:13 +0900

    chapter09
    chapter09

---
+++ src/chapter09/AnonymousExample2.java
@@ -0,0 +1,24 @@
+package chapter09;
+
+public class AnonymousExample2 {
+	public static void main(String[] args) {
+		Anonymous2 anony = new Anonymous2();
+		
+		anony.field.wake();
+		
+		anony.method1();
+		
+		anony.method2(
+			new Person() {
+				void study() {
+					System.out.println("공부합니다.");
+				}
+				
+				void wake() {
+					System.out.println("8시에 일어납니다.");
+					study();
+				}
+			}
+		);
+	}
+}
